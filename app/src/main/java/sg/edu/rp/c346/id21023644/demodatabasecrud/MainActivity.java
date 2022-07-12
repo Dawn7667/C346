2d3d6adb64b0ebe2c081d12ef92ce8ac070c2f19
TL <21023644@myrp.edu.sg>
2022-07-12 11:06:32 +0800

    First Commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21023644/demodatabasecrud/MainActivity.java
@@ -0,0 +1,28 @@
+package sg.edu.rp.c346.id21023644.demodatabasecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public long insertNote(String noteContent) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NOTE_CONTENT, noteContent);
+        long result = db.insert(TABLE_NOTE, null, values);
+        db.close();
+        Log.d("SQL Insert","ID:"+ result); //id returned, shouldn’t be -1
+        return result;
+    }
+
+}
